2d0aa3d60cb92ae2b192fe827b28d32ecbcbc718
Gopal Othayoth <cerebraldatabank@tamu.edu>
2025-01-25 18:55:50 -0600

    Add more changes, UI structure complete

--- src/app/add-pics/page.tsx
+++ src/app/add-pics/page.tsx
@@ -9,11 +9,11 @@ export default function Page() {
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream) => {
+        .then(stream => {
           video.srcObject = stream;
           video.play();
         })
-        .catch((err) => {
+        .catch(err => {
           console.error("Error accessing the camera: ", err);
         });
     }
@@ -21,10 +21,24 @@ export default function Page() {
 
   return (
     <div className="root">
+      <div className="row-header">
+        <a href="/flightAttendant" className="btn btn-neutral">&#x2B05; Back</a>
+        <h1 className="text-2xl font-bold text-center">Add Lost Items</h1>
+      </div>
       <div className="row-cam">
         <video id="cam-viewfinder" className="cam-viewfinder"></video>
       </div>
-      <div className="row-mic">B</div>
+      <div className="row-mic">
+        <label className="flex items-center cursor-pointer mic-toggle-label">
+          <div className="relative">
+            <input type="checkbox" id="mic-toggle" className="sr-only" onChange={handleMicToggle} />
+            <div className="block w-14 h-8 rounded-full mic-toggle-bg"></div>
+            <div className="dot absolute top-1 bg-white w-6 h-6 rounded-full"></div>
+          </div>
+          <div className="ml-3 text-gray-700 font-medium">Toggle Microphone</div>
+        </label>
+        <div className="current-seat-num">12R</div>
+      </div>
       <div className="row-btns">
         <button className="w-full rounded-lg btn btn-neutral">Delete Last Photo</button>
         <button className="w-full rounded-lg btn btn-primary" onClick={handleAddPhoto}>Add Photo</button>
@@ -38,4 +52,8 @@ export default function Page() {
 
 function handleAddPhoto() {
   alert("yoop");
+}
+
+function handleMicToggle() {
+  // here
 }
