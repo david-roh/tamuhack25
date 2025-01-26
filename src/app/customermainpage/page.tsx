457e09d5009c5f3771cb698e47cdcab39bcaa91b
sarahlee1tamu <123109651+sarahlee1tamu@users.noreply.github.com>
2025-01-25 18:31:33 -0600

    just added routing to EMPLOYEE LOGIN

--- src/app/customermainpage/page.tsx
+++ src/app/customermainpage/page.tsx
@@ -52,6 +52,7 @@ export default function Home() {
 
         <nav className="space-y-4">
           {[
+            { title: "EMPLOYEE LOGIN", href: "/toren" },
             { title: "Track your bags", href: "/track-bags" },
             { title: "I lost something", href: "/lost-items" },
             { title: "Wi-Fi and free entertainment", href: "/wifi" },
